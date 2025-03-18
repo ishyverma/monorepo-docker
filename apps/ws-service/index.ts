4e6dbfd13cb5239dbcb053b9b9ee050cdd6820e7
ishyverma <ishyamver@gmail.com>
2025-03-18 22:23:18 +0530

    completed every service

--- apps/ws-service/index.ts
+++ apps/ws-service/index.ts
@@ -1,7 +1,14 @@
+import prisma from "@repo/db/client";
 import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", socket => {
-  
+wss.on("connection", async socket => {
+  const user = await prisma.user.create({
+    data: {
+      username: Math.random().toString(),
+      password: Math.random().toString()
+    }
+  })
+  socket.send(JSON.stringify(user))
 })
